a1a8a268822dc3b7257b1f85722f6f88332d8990
Gkristof96 <kristef.g@gmail.com>
2021-06-20 19:09:23 +0200

    footer mobil nézet kicsinyítése

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   drawer.classList.toggle("open");
 });
+
+const navs = document.querySelectorAll(".footer-navlist");
+const title = document.querySelectorAll(".nav-title");
+
+title.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    let rank = event.target.getAttribute("rank");
+    navs[rank].classList.toggle("open");
+  });
+});
