8e5410cd29f3641e70a77cf6b177eead3cd19aa4
Gkristof96 <kristef.g@gmail.com>
2021-06-20 19:44:12 +0200

    mobil navigációs menü kész

--- main.js
+++ main.js
@@ -12,6 +12,17 @@ const title = document.querySelectorAll(".nav-title");
 title.forEach((element) => {
   element.addEventListener("click", (event) => {
     let rank = event.target.getAttribute("rank");
+    console.log(rank);
     navs[rank].classList.toggle("open");
+  });
+});
+
+const drawerNav = document.querySelectorAll(".expand");
+const dropDown = document.querySelectorAll(".side-dropdown");
+
+drawerNav.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    let rank = event.target.getAttribute("rank");
+    dropDown[rank].classList.toggle("open");
   });
 });
